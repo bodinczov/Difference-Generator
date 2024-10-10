3c95e591b8c7755023c0caec2981a56da2576e76
bodinczov <boris.od.251@mail.ru>
2024-10-10 12:34:14 +0300

    linter warnings fixed

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,12 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
 
-const ast = (filesDif, formatType) => {
+const ast = (filesDif, formatType,) => {
   switch (formatType) {
-    case 'plain': return (plain(filesDif));
-    case 'json': return (JSON.stringify(filesDif));
-    case 'stylish': return (stylish(filesDif));
-    default: throw new Error(`Unknown format: '${formatType}'!`);
+  case 'plain': return (plain(filesDif,));
+  case 'json': return (JSON.stringify(filesDif,));
+  case 'stylish': return (stylish(filesDif,));
+  default: throw new Error(`Unknown format: '${formatType}'!`,);
   }
 };
 
